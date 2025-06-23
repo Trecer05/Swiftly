6c94b65515002e7a4fac544dc91909d07dc2abf8
Linares <akuruma025@yandex.ru>
2025-06-24 00:38:11 +0300

    add chat db manager

---
+++ internal/repository/postgres/chat/manager.go
@@ -0,0 +1,37 @@
+package postgres
+
+import (
+	"database/sql"
+	"fmt"
+	"sync"
+)
+
+type Manager struct {
+	Conn *sql.DB
+	WG   *sync.WaitGroup
+	MU   *sync.RWMutex
+}
+
+func NewChatManager(driverName string, sourceName string) *Manager {
+	db, err := sql.Open(driverName, sourceName)
+	if err != nil {
+		panic(fmt.Sprintf("Не удалось подключиться к базе данных: %v", err))
+	}
+
+	if err = db.Ping(); err != nil {
+		panic(fmt.Sprintf("База данных не отвечает: %v", err))
+	}
+
+	return &Manager{
+		Conn: db,
+		WG:   &sync.WaitGroup{},
+		MU:   &sync.RWMutex{},
+	}
+}
+
+func (manager *Manager) Close() {
+	if manager.Conn != nil {
+		manager.Conn.Close()
+		manager.Conn = nil
+	}
+}
