ecf1da1397710a80281b8a1227196e84eea375e7
Linares <akuruma025@yandex.ru>
2025-07-01 13:08:09 +0300

    fix access token expire error in middleware

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -42,7 +42,7 @@ func AuthMiddleware() mux.MiddlewareFunc {
 			if err != nil {
 				w.Header().Set("Content-Type", "application/json")
 				w.WriteHeader(http.StatusUnauthorized)
-				json.NewEncoder(w).Encode(map[string]string{"error": "invalid token: " + err.Error()})
+				json.NewEncoder(w).Encode(map[string]string{"error": tokenErrors.ErrAccessTokenExpired.Error()})
 				log.Println("Invalid token:", err)
 				return
 			}
