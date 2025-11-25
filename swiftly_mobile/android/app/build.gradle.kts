87d5ea7f6cd6a9393823b464b450a454c5d22fc0
DonaterSam <savoys.89wax@icloud.com>
2025-11-26 01:55:49 +0300

    grandle_up

--- swiftly_mobile/android/app/build.gradle.kts
+++ swiftly_mobile/android/app/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 
 android {
     namespace = "com.example.swiftly_mobile"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 36
+    ndkVersion = "29.0.13846066"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
